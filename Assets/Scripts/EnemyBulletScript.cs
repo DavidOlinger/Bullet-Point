a8e45a57e99054f0904e852d9b39284ed3806f70
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2024-09-18 12:10:01 -0400

    FixedUpdate Implementation and Rotation support on enemy bullets

--- Assets/Scripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyBulletScript.cs
@@ -12,38 +12,50 @@ public class EnemyBulletScript : MonoBehaviour
     private Vector2 baseVector; 
     Vector2 dirVector; //direction vector 
     public float forceAngle = 0f; //angle of movement
+    public bool targetingPlayer = false;
 
-    public float updatesPerSecond = 50f; // How many times per second the force pull should occur
-    private float dtAccumulator = 0f;    // to correct for low framerates. stores up delta time to apply force consistently
-    private float fixedTimestep; //inverse of updates per second
 
 
     // Start is called before the first frame update
     void Start()
     {
-        fixedTimestep = 1f / updatesPerSecond; //setting starting values
+        // fixedTimestep = 1f / updatesPerSecond; //setting starting values
         baseVector = new Vector2(0, 1);
-        dirVector = vecRotate(baseVector, forceAngle);
-        bulletBody = GetComponent<Rigidbody2D>(); //finding instances of components
 
-        bulletBody.AddForce(dirVector * velCoef); // add starting velocity
+        if (targetingPlayer) //add option for a bullet to spawn with its rotation set to point at the player.
+        { 
+            // GameObject player reference
+            // 
+            // Vector3 directionToTarget = player.transform.position - transform.position;
+            // Get the angle using Vector3.Angle
+            // float angleToPlayer = Vector3.Angle(transform.forward, directionToTarget);
+        }
+
+        dirVector = vecRotate(baseVector, forceAngle);
+        transform.Rotate(Vector3.forward, forceAngle);
+        bulletBody = GetComponent<Rigidbody2D>(); //finding instances of components
+        bulletBody.velocity = dirVector * velCoef;
+
+        // add starting velocity
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        dtAccumulator += Time.deltaTime;
-        accCoef += Time.deltaTime * jerkCoef; //jerk the acceleration
-        while (dtAccumulator >= fixedTimestep) //while there is time stored up for physics updates
-        {
-            bulletBody.AddForce(dirVector * accCoef); // accelerate the bullet
-            dtAccumulator -= fixedTimestep; //spend the stored up time
-        }
+
     }
 
-    Vector2 vecRotate(Vector2 inVector, float angleDelta) // Magical Math Machine
+    void FixedUpdate()
     {
-        Vector2 outVector = Quaternion.AngleAxis(angleDelta, Vector3.forward) * inVector; 
+        bulletBody.velocity = dirVector * velCoef;
+        velCoef += accCoef / 50; // accelerate the bullet
+        accCoef += jerkCoef / 50; // Increase acceleration
+    }
+
+    Vector2 vecRotate(Vector2 inVector, float angleDelta) // Rotates inVector and returns the rotated version
+    {
+        Vector2 outVector = Quaternion.AngleAxis(angleDelta, Vector3.forward) * inVector;
         return outVector;
     }
 }
