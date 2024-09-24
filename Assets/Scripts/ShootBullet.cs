896c77bf4e7f1102aa8a101408169127627ad5f0
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2024-09-24 00:01:25 -0400

    Updated bullet shooting scripts with spread support

--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -39,4 +39,7 @@ public class ShootBullet : MonoBehaviour
             
         }
     }
+
+
 }
+
--- Assets/Scripts/TriangleMoveAndShoot.cs
+++ Assets/Scripts/TriangleMoveAndShoot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class TriangleMoveAndShoot : MonoBehaviour
 
     public float moveSpeed;
     Rigidbody2D rb;
+    public int numBullets;
+    public float spread;
+    public float direction;
+    public int numWaves;
+    public float waveDelay;
 
     public GameObject bullet;
     public float bulletSpawnPos = 1;
@@ -37,19 +43,51 @@ public class TriangleMoveAndShoot : MonoBehaviour
 
         if (timeSinceLastSpawn > spawnCooldown)
             {
-                Vector3 playerPosition = transform.position;
+            //Vector3 playerPosition = transform.position;
 
-                Vector3 spawnPosition = playerPosition + new Vector3(0, bulletSpawnPos, 0);
+            //Vector3 spawnPosition = playerPosition + new Vector3(0, bulletSpawnPos, 0);
 
-                Instantiate(bullet, spawnPosition, Quaternion.identity);
+            //Instantiate(bullet, spawnPosition, Quaternion.identity);
+            StartCoroutine(FireBulletSpread(numBullets, spread, direction, numWaves, waveDelay));
 
                 timeSinceLastSpawn = 0;
             }
 
     }
 
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveSpeed, 0);
     }
+
+    IEnumerator FireBulletSpread(int numBullets, float spread, float direction, int numWaves, float waveDelay) //this is special!
+        //this is a coroutine, which means we can manage time with it. This is powerful but the implementation is a bit strange.
+        //look up some documentation on it when you get the chance.
+    {
+        Vector3 playerPosition = transform.position;
+
+        Vector3 spawnPosition = playerPosition + new Vector3(0, bulletSpawnPos, 0);
+        direction += 180; //FIXME
+        for (int i = 0; i < numWaves; i++)
+        {
+            for (int j = 0; j < numBullets; j++)
+            {
+                float fireAngle = direction + spread * ((2f * j / (numBullets - 1)) - 1f);
+                GameObject firedBullet = Instantiate(bullet, spawnPosition, Quaternion.identity);
+                EnemyBulletScript fbScript = firedBullet.GetComponent<EnemyBulletScript>();
+                if (fbScript != null)
+                {
+                    fbScript.forceAngle = fireAngle;
+                } else
+                {
+                    Debug.Log("Failed to obtain bullet reference.");
+                    yield break;
+                }
+            }
+            yield return new WaitForSeconds(waveDelay);
+        }
+       
+    }
+
 }
